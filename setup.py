45363b661185e721b499c74006d6a61a7e73f363
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-05-17 19:53:50 -0600

    Updating sub-version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='QTestLibrary',
-    version='0.2',
+    version='0.2.1',
     packages=['qtest'],
     license='ISC',
     long_description=open('README.md').read(),
